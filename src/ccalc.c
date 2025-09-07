f2ce84e7699a43ec00a10e06aefe99efab2fb405
aarcticc <hcf2509@proton.me>
2025-09-07 21:01:19 +0200

    nothing changed

--- src/ccalc.c
+++ src/ccalc.c
@@ -3,16 +3,13 @@
 #include "defs.h"
 
 int main(){
-    int beginning;
-    double number[NUMBER_LENGTH][NUMBER_AMOUNT]; 
-    char* input;
+    char input;
 
     // calc logic
     printf("What do you want to calculate: \n");
     while (input < NUMBER_AMOUNT || input < NUMBER_LENGTH){
-        scanf(" %c", input);
+        
     }
 
     return 0;
 }
-
